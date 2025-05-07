aaf7e591dd4b1afba9944855435400f074efb047
João Pedro Brasil <134008860+dubicofmg@users.noreply.github.com>
2025-05-07 16:04:56 -0300

    Add files via upload

---
+++ app_clientes_cadastro_nstech.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import pandas as pd
+
+# Carregando os dados
+df = pd.read_excel("C:/Users/JoãoPedroBrasildeAra/OneDrive - NSTECH GR LTDA/Main/Base Clientes Nstech - Cadastro/Fonte_bi_clientes_cadastro.xlsx")  # ou csv, ou qualquer origem
+
+st.title("Dashboard Interativo")
+
+# Cria filtros para cada coluna desejada
+col1, col2, col3, col4, col5 = st.columns(5)
+
+with col1:
+    filtro_cliente = st.multiselect("Nome Cliente", df["NOME_CLIENTE"].unique())
+
+with col2:
+    filtro_cnpj = st.multiselect("CNPJ", df["CNPJ_CLIENTE"].unique())
+
+with col3:
+    filtro_raiz = st.multiselect("Raiz", df["raiz"].astype(str).unique())
+
+with col4:
+    filtro_dom = st.multiselect("Domínio", df["Emails"].unique())
+
+with col5:
+    filtro_emp = st.multiselect("Tipo intersecção", df["Tipo Intersecção"].astype(str).unique())
+
+# Aplicando os filtros
+df_filtrado = df.copy()
+
+if filtro_cliente:
+    df_filtrado = df_filtrado[df_filtrado["NOME_CLIENTE"].isin(filtro_cliente)]
+if filtro_cnpj:
+    df_filtrado = df_filtrado[df_filtrado["CNPJ_CLIENTE"].isin(filtro_cnpj)]
+if filtro_raiz:
+    df_filtrado = df_filtrado[df_filtrado["raiz"].astype(str).isin(filtro_raiz)]
+if filtro_dom:
+    df_filtrado = df_filtrado[df_filtrado["Emails"].isin(filtro_dom)]
+if filtro_emp:
+    df_filtrado = df_filtrado[df_filtrado["Tipo Intersecção"].astype(str).isin(filtro_emp)]
+
+df_filtrado = df_filtrado.drop(df_filtrado.columns[[4,5,6,7,8,9]], axis=1)
+
+# Mostra o resultado
+st.dataframe(df_filtrado, use_container_width=True)
