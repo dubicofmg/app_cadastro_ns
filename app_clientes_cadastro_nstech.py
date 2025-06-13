a2f570e2255bb7fe4c29ffe4cd7aceb35789b822
João Pedro Brasil <134008860+dubicofmg@users.noreply.github.com>
2025-06-13 14:22:30 -0300

    Update app_clientes_cadastro_nstech.py

--- app_clientes_cadastro_nstech.py
+++ app_clientes_cadastro_nstech.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import pandas as pd
 import streamlit_authenticator as stauth
-import streamlit as st
 
-st.write("streamlit-authenticator version:", stauth.__version__)
+
 
 
 # ==========================
